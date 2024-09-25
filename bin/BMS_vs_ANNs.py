234573d5495484fca758bcf466e031e35072d049
SergioCoboLopez <srgcobo@gmail.com>
2024-09-25 10:05:48 +0200

    removed old traces

--- bin/BMS_vs_ANNs.py
+++ bin/BMS_vs_ANNs.py
@@ -19,21 +19,97 @@ N=9 #n goes from 0 to 9
 #Test case
 n=0
 sigma=0.14
+steps=50000
+NPAR=10
 functions=['tanh', 'leaky_ReLU']
 realizations=2
+function=functions[0]
+realization=0
+
 
 #Read ann data
 #----------------------------------------------
 file_ann='NN_no_overfit_' + function + '_sigma_' + str(sigma) + '_r_' + str(realization) + '.csv'
-model_d='../data/' + file_model
+model_d='../data/' + file_ann
 d=pd.read_csv(model_d)
 dn=d[d['rep']==n]
-dn.set_index('Unnamed: 0', inplace=True)
-dn.index.name = None
-dn=dn.reset_index(drop=True)
+dn.set_index('Unnamed: 0', inplace=True);dn.index.name = None;dn=dn.reset_index(drop=True)
 #----------------------------------------------
 
 #Read BMS data
+#----------------------------------------------
 filename='BMS_'+function+'_n_'+str(n)+'_sigma_'+str(sigma)+ '_r_' + str(realization) + '_trace_'+str(steps)+'_prior_'+str(NPAR)+ '.csv'
 trace=pd.read_csv('../data/MSTraces/' + filename, sep=';', header=None, names=['t', 'H', 'expr', 'parvals', 'kk1', 'kk2','kk3'])
 
+#Parameters to read models
+VARS = ['x1',]
+x = dn[[c for c in VARS]].copy()
+y=dn.noise
+
+prior_par = read_prior_par('machine-scientist/Prior/final_prior_param_sq.named_equations.nv1.np10.2017-10-18 18:07:35.089658.dat')
+
+#mdl model
+minrow = trace[trace.H == min(trace.H)].iloc[0]
+minH, minexpr, minparvals = minrow.H, minrow.expr, ast.literal_eval(minrow.parvals)
+
+t = Tree(
+    variables=list(x.columns),
+    parameters=['a%d' % i for i in range(NPAR)],
+    x=x, y=y,
+    prior_par=prior_par,
+    max_size=200,
+    from_string=minexpr,
+)
+
+t.set_par_values(deepcopy(minparvals))
+
+dplot = deepcopy(dn)
+dplot['ybms'] = t.predict(x)
+#----------------------------------------------
+
+
+#Errors
+#----------------------------------------------
+MSE_nn_train=mean_squared_error(dplot.loc[:train_size-1]['ymodel'],dplot.loc[:train_size -1]['y'])
+MSE_nn_test=mean_squared_error(dplot.loc[train_size-1:]['ymodel'],dplot.loc[train_size -1:]['y'])
+
+MSE_mdl=mean_squared_error(dplot.ybms,dn.y)
+MSE_mdl_train=mean_squared_error(dplot.loc[:train_size-1]['ybms'],dn.loc[:train_size-1]['y'])
+MSE_mdl_test =mean_squared_error(dplot.loc[train_size-1:]['ybms'],dn.loc[train_size-1:]['y'])
+#----------------------------------------------
+
+#Figure
+#-------------------------------------------------
+cm = 1/2.54 #convert inch to cm                                                                     
+width = 10*cm; height=8*cm
+
+Extensions=['.png', '.pdf']
+
+#Fonts and sizes                                                                                    
+size_axis=7;size_ticks=6;size_title=5
+line_w=1;marker_s=3 #width and marker size
+
+fig=figure(figsize=(width,height), dpi=300)                                                        
+
+plt.plot(dn['x1'], dn['ymodel'],linewidth=line_w, color='red', label='ann.  MSE_train= %.2E, MSE_test= %.2E' % ( MSE_nn_train, MSE_nn_test)) 
+plt.plot(dplot.x1, dplot.ybms, color='green', linestyle='--',label='mdl.  MSE_train= %.2E, MSE_test= %.2E' % ( MSE_mdl_train, MSE_mdl_test)) 
+plt.plot(dn['x1'], dn['y'],'.', markersize=2, color='blue',label='observed') 
+plt.plot(dn['x1'], dn['y_noise'],linewidth=line_w, markersize=2, color='orange',label='noise, ' + r'$\sigma$=' + str(sigma) )
+
+plt.axvline(x=validation_border,linestyle='--',linewidth=line_w, color='r')
+plt.axvline(x=train_border,linestyle='--',linewidth=line_w, color='k')
+
+#plt.title('$%s$' % t.latex(),fontsize=size_title)
+plt.title('n=%d, r=%d : $%s$' % (n, realization, t.latex()),fontsize=size_title)
+plt.xlabel('x',fontsize=size_axis);plt.ylabel('y',fontsize=size_axis)
+plt.xticks(fontsize=size_ticks);plt.yticks(fontsize=size_ticks)
+plt.legend(loc='best', fontsize=size_ticks)
+
+plt.xlim(-2,2);plt.ylim(-0.5,1.5)                                               
+
+Name_figure='Prediction_' + function + '_n_' + str(n) + '_sigma_' + str(sigma) + '_r_'  + str(realization) + '_par_' + str(NPAR)
+    
+# for ext in Extensions:
+#         plt.savefig('../results/' + Name_figure +ext,dpi=300)
+
+plt.show()
